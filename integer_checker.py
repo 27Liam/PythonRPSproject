6819e63f3e2c3049ca78439e71f9e07c40a8bdd1
27Liam <114444535+27Liam@users.noreply.github.com>
2024-06-13 09:31:13 +1200

    Add files via upload

--- integer_checker.py
+++ integer_checker.py
@@ -1,10 +1,16 @@
 
 while True:
 
+    error = "Please enter an integer that is 13 or more"
+
     try:
 
         my_num = int(input("Enter an integer"))
-        print("Your number is ", my_num)
+
+        if my_num < 13:
+            print(error)
+        else:
+            print("Your number is ", my_num)
 
     except ValueError:
-        print("Please enter an integer")
+        print(error)
